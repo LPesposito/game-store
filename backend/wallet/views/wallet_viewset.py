a4a48a7f9adbfb0e13446c43851a56007ca0688a
Luan Esposito <luanesposito2003@gmail.com>
2025-06-15 20:40:44 -0300

    :sparkles: feat: Enhance wallet functionality with add and withdraw actions, including validation and transaction logging

--- backend/wallet/views/wallet_viewset.py
+++ backend/wallet/views/wallet_viewset.py
@@ -3,22 +3,61 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from wallet.models import Wallet
 from wallet.serializers import WalletSerializer
+from wallet.models import Transaction
 
 class WalletViewSet(viewsets.ViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
-    def retrieve(self, request):
-        wallet = Wallet.objects.get(user=request.user)
-        serializer = WalletSerializer(wallet)
-        return Response(serializer.data)
 
-    @action(detail=False, methods=['post'])
+    @action(detail=False, methods=['post'], url_path='add-funds')
     def add_funds(self, request):
+        wallet = self.get_object()
         amount = request.data.get('amount')
-        if not amount:
-            return Response({'error': 'Amount is required'}, status=400)
 
-        wallet = Wallet.objects.get(user=request.user)
-        wallet.balance += float(amount)
+        try:
+            amount = float(amount)
+        except (ValueError, TypeError):
+            return Response({'error': 'Valor inválido.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if amount <= 0:
+            return Response({'error': 'O valor deve ser positivo.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        wallet.balance += amount
         wallet.save()
-        return Response({'message': 'Funds added', 'new_balance': wallet.balance}, status=200)
+
+        
+        Transaction.objects.create(
+            wallet=wallet,
+            transaction_type='deposit',
+            amount=amount,
+            note='Adição de fundos'
+        )
+
+        return Response({'message': 'Fundos adicionados com sucesso.', 'balance': wallet.balance})
+
+
+    @action(detail=False, methods=['post'], url_path='withdraw')
+    def withdraw(self, request):
+        wallet = self.get_object()
+        amount = request.data.get('amount')
+
+        try:
+            amount = float(amount)
+        except (ValueError, TypeError):
+            return Response({'error': 'Valor inválido.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if amount <= 0 or amount > wallet.balance:
+            return Response({'error': 'Valor inválido ou saldo insuficiente.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        wallet.balance -= amount
+        wallet.save()
+
+       
+        Transaction.objects.create(
+            wallet=wallet,
+            transaction_type='withdraw',
+            amount=amount,
+            note='Retirada de fundos'
+        )
+
+        return Response({'message': 'Fundos retirados com sucesso.', 'balance': wallet.balance})
